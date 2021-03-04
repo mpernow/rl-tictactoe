b651381d025f48e780e569e3b29c1ab7f53ed670
Marcus Pernow <marcus.pernow@gmail.com>
2021-03-04 22:51:44 +0100

    learning params updated

--- params.py
+++ params.py
@@ -3,12 +3,11 @@ lose_reward = -1.
 draw_reward = 0.1
 
 learning_rate = 0.5
-discount_factor = 0.8
+discount_factor = 0.7
 
 # Exploration rate minimum, maximum, and decay rate:
-#exploration_rate = 0.1
 eps_min = 0.01
-eps_max = 1.
-tau = 0#0.001 # quite slow, since large state space
+eps_max = 0.5
+tau = 0.0001 # quite slow, since large state space
 
 f_name = 'q.pkl'
